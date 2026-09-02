2d7e96a2ea46779c587049d747aee796811c904c
Kemal Uludağ <kemal.uludag@sabanciuniv.edu>
2026-09-02 15:54:26 +0300

    Add initial routing data and distance calculation

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("RouteIQ başladı")
+from src.problem_data import depot, customers
+from src.distance import calculate_distance
+
+
+print(depot)
+print(customers)
+
+
+distance = calculate_distance(depot, customers[0])
+
+print(distance)
---
+++ src/distance.py
@@ -0,0 +1,5 @@
+import math
+def calculate_distance(point1, point2):
+        x1, y1 = point1
+        x2, y2 = point2
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
